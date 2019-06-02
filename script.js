9240a7bb47e755097fad10b1814986dc77157267
ValdekBro <profittom5@gmail.com>
2019-06-02 18:57:34 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var id = 0;
 
+
+//Add new button click handler
+var id = 0;
 $('#addCarButton').click( () => {
     id++;
     $('#right-tab').append($('<div></div>').addClass('box border')
@@ -7,27 +9,35 @@ $('#addCarButton').click( () => {
     .append($('<div></div>').addClass('title').text($('#modelInput').val()))
     .append($('<div></div>').addClass('label').text($('#colorInput').val()))
     .append($('<div></div>').addClass('label').text($('#priceInput').val()))
-    .append($('<button></button>').addClass('right').text('Rent').click(function() {
-        let date = new Date();
-        let term = prompt('How long (hours) do u want to use the car?', 24);
-        if(term == null) return;
-        date.setSeconds(date.getSeconds() + parseInt(term, 10));
-        $(this).parent().attr('date', date);
-        $(this).parent().children().hide();
-        $(this).parent().addClass('rented');
-        $(this).parent().append($('<div></div>').text('Rented to ' + $(this).parent().attr('date')).attr('id', 'rent'));
-    }))
-    .append($('<button></button>').addClass('right').text('Delete').click(function() {
-        $(this).parent().remove();
-    }))
+    .append(
+        $('<button></button>').addClass('right').text('Rent')
+        .click(function() {
+            let date = new Date();
+            let term = prompt('How long (hours) do u want to use the car?', 24);
+            date.setSeconds(date.getSeconds() + parseInt(term, 10));
+            if(date == 'Invalid Date') return;
+            $(this).parent().attr('date', date);
+            $(this).parent().children().hide();
+            $(this).parent().addClass('rented');
+            $(this).parent().append($('<div></div>').text('Rented to ' + $(this).parent().attr('date')).attr('id', 'rent'));
+        })
+    )
+    .append(
+        $('<button></button>').addClass('right').text('Delete')
+        .click(function() {
+            $(this).parent().remove();
+        })
+    )
     );
 });
 
+//Search
 $('#searchInput').change(function() {
     $("#right-tab").children().hide();
     $( ".box:contains('"+ $('#searchInput').val() +"')" ).show();
 })
 
+//Rent term lost checking
 setInterval(()=>{   
     $('.box').each(function( index ) {
         if((Date.parse($(this).attr('date'))) < (new Date())) {
@@ -38,3 +48,8 @@ setInterval(()=>{
         };
     });
 }, 100)
+
+
+$('#searchInput').tooltip({ 
+    content: "Awesome title!"
+});
